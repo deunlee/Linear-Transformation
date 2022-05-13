301ebd5b0f9c7929747c7fc64cd83315b79d6ce4
Deun Lee <54697735+deunlee@users.noreply.github.com>
2022-05-13 22:57:28 +0900

    Add Vue and Quasar

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <q-card
+        class="img-card noselect"
+        :class="{ 'glowing-item': selected }"
+        v-ripple:white
+    >
+        <q-img :src="getImageUrl(image)">
+            <div class="img-card-title absolute-bottom text-h6" v-if="title">
+                {{ title }}
+            </div>
+        </q-img>
+    </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ImageCard',
+    props: {
+        title: {
+            type: String,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        selected: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup() {
+        const getImageUrl = (image: string) => {
+            return require('../assets/' + image);
+        };
+
+        return {
+            getImageUrl,
+        };
+    },
+});
+</script>
+
+<style scoped>
+.img-card {
+    min-width: 200px;
+    cursor: pointer;
+}
+
+.img-card-title {
+    background: rgba(147, 147, 147, 0.6);
+}
+
+/* https://cssbud.com/css-generator/css-glow-generator/ */
+.glowing-item {
+    -webkit-box-shadow: 0px 0px 11px 2px rgba(250, 117, 0, 0.9);
+    -moz-box-shadow: 0px 0px 11px 2px rgba(250, 117, 0, 0.9);
+    box-shadow: 0px 0px 11px 2px rgba(250, 117, 0, 0.9);
+    transition: box-shadow 0.3s;
+}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,59 @@
+<template>
+    <q-page class="row items-center justify-evenly">
+        <image-card
+            v-for="(card, index) in cards"
+            :key="index"
+            v-bind="card"
+            @click="selectCard(index)"
+        ></image-card>
+    </q-page>
+</template>
+
+<script lang="ts">
+import ImageCard from 'components/ImageCard.vue';
+import { defineComponent, reactive } from 'vue';
+
+export default defineComponent({
+    name: 'MainPage',
+    components: { ImageCard },
+    setup() {
+        const cards = reactive([
+            {
+                title: 'Shearing',
+                image: 'canvas.png',
+                selected: false,
+                matrix: [2, 1, 1, 2],
+            },
+            {
+                title: 'Rotation',
+                image: 'canvas.png',
+                selected: false,
+                matrix: [0, -1, 1, 0],
+                // [cos(pi/2), -sin(pi/2), sin(pi/2), cos(pi/2)]
+            },
+            {
+                title: 'Permutation',
+                image: 'canvas.png',
+                selected: false,
+                matrix: [0, 1, 1, 0],
+            },
+            {
+                title: 'Projection',
+                image: 'canvas.png',
+                selected: false,
+                matrix: [1, 2, 2, 4], // projection on a vector [1, 2]
+                // x-axis [1, 0, 0, 0]
+            },
+        ]);
+
+        const selectCard = (index: number) => {
+            cards.forEach((e) => (e.selected = false));
+            cards[index].selected = true;
+        };
+
+        selectCard(0);
+
+        return { cards, selectCard };
+    },
+});
+</script>
